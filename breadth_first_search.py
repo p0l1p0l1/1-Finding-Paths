5768e2e9709cbbceb66e73ecabcdc06d193bd16d
p0l1p0l1 <149327963+p0l1p0l1@users.noreply.github.com>
2023-11-09 15:02:23 +0100

    (Updated) breadth_first_search.py

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -1,3 +1,4 @@
+import string
 wall = "⬛"
 empty = "🟩"
 full = "❎"
@@ -13,22 +14,31 @@ labyrinth = [
 "⬛🟩🟩🟩🟩🟩⬛🟩🟩🟦",
 "⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛"]
 
-#start = (int(input("Starting row: ")), int(input("Starting column: ")))
+def print_labyrinth(lab: list[str], path: list[tuple[int, int]] = None):
+    if path != None:
+       for i in path:
+            lab[i[0]]= lab[i[0]][:i[1]] + full + lab[i[0]][i[1]+1:]
+    numbers= []
+    for i in range(len(lab)):
+        numbers.append(str(i))
+    print("   "+" ".join(numbers))
+    for i in range(len(lab)):
+        print(str(i)+" "+lab[i])
+    print("")
 
+def prompt_integer(position: str, message: str) -> int:
+    integer = input(f"{position} of {message}: ")
+    while integer.isdigit() != True:
+        integer = input()
+    return int(integer)
 
-def bfs(lab, start=(1,1), end=(8,8)):
-        #printing the unsolved labyrinth
-        print("Original labyrinth:\n")
-        numbers= []
-        for i in range(len(lab)):
-            numbers.append(str(i))
-        print("   "+" ".join(numbers))
-
-        for i in range(len(lab)):
-            print(str(i)+" "+lab[i])
-
-        print("")
-
+def prompt_user_for_location(name: str) -> tuple[int, int]:
+    row = prompt_integer("Row", name)
+    col = prompt_integer("Column", name)
+    print("")
+    return (row, col)    
+            
+def bfs(lab: list[str], start: tuple[int, int] = (1, 1), end: tuple[int, int] = (8, 8)) -> list[tuple[int, int]]:
         #defining things for later
         been_there = []
         paths = [[start]]
@@ -38,7 +48,7 @@ def bfs(lab, start=(1,1), end=(8,8)):
         while end not in been_there:
             #trying all avaiable paths
             for i in range(len(paths)):
-                if paths[i][-1] != "Dead End":
+                if paths[i][-1] != False:
                     path_options = []
                     #checking how each path could extend
                     for j in moves:
@@ -46,41 +56,29 @@ def bfs(lab, start=(1,1), end=(8,8)):
                             path_options.append((paths[i][-1][0]+j[0], paths[i][-1][1]+j[1]))
                     been_there.extend(path_options)
 
-                    #extending paths + crearing new if there is a fork
+                    #extending paths + creating new if there is a fork
                     if len(path_options)>2:
                         paths.append([])
                         paths.append([])
                         paths[-2]=paths[i]+[path_options[2]]
                         paths[-1]=paths[i]+[path_options[1]]
                         paths[i].append(path_options[0])
-
                     elif len(path_options)==2:
                         paths.append([])
                         paths[-1]=paths[i]+[path_options[1]]
                         paths[i].append(path_options[0])
-
-
                     elif len(path_options) == 1:
                         paths[i].append(path_options [0])
                     else:
-                         paths[i].append("Dead End")
+                         paths[i].append(False)
+
         #finding first complete path (if there are multiple)
         for i in range(len(paths)):
             if end in paths[i]:
                 path = paths[i]
-                break
-        #printing the solution
-        print("The fastest solution:\n")
-        solution = lab
-        for i in path:
-            solution[i[0]]= solution[i[0]][:i[1]] + full + solution[i[0]][i[1]+1:]
+                return path
 
-        numbers= []
-        for i in range(len(solution)):
-            numbers.append(str(i))
-        print("   "+" ".join(numbers))
-
-        for i in range(len(solution)):
-            print(str(i)+" "+solution[i])
-
-bfs(labyrinth)
+print_labyrinth(labyrinth)
+start = prompt_user_for_location("start")    
+end = prompt_user_for_location("end") 
+print_labyrinth(labyrinth, bfs(labyrinth, start, end))
